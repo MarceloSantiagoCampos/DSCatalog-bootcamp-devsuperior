5aa8365c0c94967c77234ce73937657edb63aa9e
Marcelo Santiago <marcelosantiagocampos@gmail.com>
2021-02-02 18:52:16 -0300

    DataBase seeding

--- backend/src/main/java/com/devsupeior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsupeior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsupeior.dscatalog.entities.Category;
 import com.devsupeior.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true) // Não há a necessidade de travar o banco de dados somente para fazer a leitura, portanto é colocado o readOnly
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
