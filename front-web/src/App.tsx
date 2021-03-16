d5c217194270972946bf20d4ca83a9ab8d3b47e5
Marcelo Santiago <marcelosantiagocampos@gmail.com>
2021-03-16 19:55:06 -0300

    Add bootstrap and Sass

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <h1>Hello App</h1>
-  );
+const App  = () => {
+    return <h1>Hello App!</h1>
 }
+
 
 export default App;
