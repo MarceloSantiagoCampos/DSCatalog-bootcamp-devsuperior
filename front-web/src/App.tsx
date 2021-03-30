3f4e84a7a0d17205fd2a363b23cdd65cc3f59a04
Marcelo Santiago <marcelosantiagocampos@gmail.com>
2021-03-30 19:27:12 -0300

    useState, UseEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,36 @@
-import Alert from './Alert';
 
-
+import { useState, useEffect } from 'react';
 
 const App  = () => {
+
+    const [counter, setCounter] = useState(0);
+    // [ variavel, função set da variavel] = useState ( valor Default)
+
+    useEffect(() => {
+        console.log('O contador mudou de valor');
+
+    }, [counter])
+
+
     return (
         
         <div className="container mt-5">
-            <Alert text="Teste 1" />
-            <Alert />
-            <Alert text="Teste 3" />
+            <button 
+            className="btn btn-primary mr-5"
+            onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button 
+            className="btn btn-primary ml-5"
+            onClick={() => setCounter(counter - 1)}
+            >
+                -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
         </div>
         
     )
