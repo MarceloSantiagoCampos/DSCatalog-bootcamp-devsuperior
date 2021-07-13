846d85498b7e58c35f5989a191d4c62499c267df
Marcelo Santiago <71225896+MarceloSantiagoCampos@users.noreply.github.com>
2021-07-13 19:08:53 -0300

    dynamically listing products

--- front-web/src/pages/catalog/index.tsx
+++ front-web/src/pages/catalog/index.tsx
@@ -1,14 +1,15 @@
 import './styles.scss';
 import ProductCard from './components/ProductCard/index';
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
 const Catalog = () => {
-    
-
-
     //quando a lista de produtos estiver disponpível popular um estado no componente e listar os produtos dinamicamente
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+
+    console.log(productsResponse);
 
     //quando o componente iniciar buscar a lista de produto
     useEffect(()=>{
@@ -18,8 +19,7 @@ const Catalog = () => {
         }
         
         makeRequest({ url:'/products', params  })
-            .then(response => console.log(response))
-        ;
+            .then(response => setProductsResponse(response.data));
     }, []);
 
     return(
@@ -28,18 +28,11 @@ const Catalog = () => {
                 Catalogo de produtos
             </h1>
             <div className="catalog-prducts">
-                <Link to="/products/1"><ProductCard /></Link>
-                <Link to="/products/2"><ProductCard /></Link>
-                <Link to="/products/3"><ProductCard /></Link>
-                <Link to="/products/4"><ProductCard /></Link>
-                <Link to="/products/5"><ProductCard /></Link>
-                <Link to="/products/6"><ProductCard /></Link>
-                <Link to="/products/7"><ProductCard /></Link>
-                <Link to="/products/8"><ProductCard /></Link>
-                <Link to="/products/9"><ProductCard /></Link>
-                <Link to="/products/10"><ProductCard /></Link>
-                <Link to="/products/11"><ProductCard /></Link>
-                <Link to="/products/12"><ProductCard /></Link>
+                {productsResponse?.content.map(product => (
+                    <Link to="/products/1" key={product.id}>
+                        <ProductCard />
+                    </Link>
+                ))}
             </div>
         </div>
     )
