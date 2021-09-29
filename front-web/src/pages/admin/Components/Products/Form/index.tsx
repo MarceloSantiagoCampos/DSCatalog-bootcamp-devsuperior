b02ec96ebd7235729a543cabdb1d3c9a6c8b6698
Marcelo Santiago <71225896+MarceloSantiagoCampos@users.noreply.github.com>
2021-09-29 19:07:37 -0300

    form creation

--- front-web/src/pages/admin/Components/Products/Form/index.tsx
+++ front-web/src/pages/admin/Components/Products/Form/index.tsx
@@ -1,12 +1,49 @@
 import BaseForm from '../../BaseForm'
 import './styles.scss'
+import { useState } from 'react';
 
 const Form = () => {
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
+
+    const handleOnChangeName = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    const handleOnChangePrice = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setPrice(event.target.value);
+    }
+
+    const handleOnChangeCategory = (event:React.ChangeEvent<HTMLSelectElement>) => {
+        setCategory(event.target.value);
+    }
+
     return(
         <BaseForm title="Cadastrar Um Produto">
             <div className="row">
                 <div className="col-6">
-                    <input type="text" className="form-control"/>
+                    <input 
+                        value={name}
+                        type="text" 
+                        className="form-control mb-5"
+                        onChange={handleOnChangeName}
+                        placeholder="Nome do Produto"
+                    />
+
+                    <select className="form-control mb-5" onChange={handleOnChangeCategory}>
+                        <option value="livros">Livros</option>
+                        <option value="computadores">Computadores</option>
+                        <option value="eletronicos">Eletronicos</option>
+                    </select>
+
+                    <input 
+                        value={price}
+                        type="text" 
+                        className="form-control"
+                        onChange={handleOnChangePrice}
+                        placeholder="Preço"
+                    />
                 </div>
             </div>
         </BaseForm>
